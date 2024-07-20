bb37b2a7acb9f00a5eb26d1bd56c4d3e7e410210
Vladyslav Bihun <bihunvv@gmail.com>
2024-07-20 19:57:51 +0300

    step 1 and 2 done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const parsedData = JSON.parse(data);
+    for (let i = 1; i <= number; i++) {
+      const contacts = createFakeContact();
+      parsedData.push(contacts);
+    }
+    fs.writeFile(PATH_DB, JSON.stringify(parsedData, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 generateContacts(5);
