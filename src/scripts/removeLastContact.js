6f837426b84e08b60a3ceed291108cbfc244cbd4
Vladyslav Bihun <bihunvv@gmail.com>
2024-07-21 13:54:34 +0300

    HW done

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const parsedData = JSON.parse(data);
+    const newDataArray = parsedData.slice(0, parsedData.length - 1);
+    await fs.writeFile(PATH_DB, JSON.stringify(newDataArray, null, 2));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
