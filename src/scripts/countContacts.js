adaf37719ec111e8a1d4feda99bebc83454d2936
Vladyslav Bihun <bihunvv@gmail.com>
2024-07-21 12:53:50 +0300

    step 6 done

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -2,9 +2,13 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 
 export const countContacts = async () => {
-  const data = await fs.readFile(PATH_DB, 'utf-8');
-  const parsedData = JSON.parse(data);
-  return parsedData.length;
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const parsedData = JSON.parse(data);
+    return parsedData.length;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,9 +2,13 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 
 export const getAllContacts = async () => {
-  const data = await fs.readFile(PATH_DB, 'utf-8');
-  const parsedData = JSON.parse(data);
-  return parsedData;
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const parsedData = JSON.parse(data);
+    return parsedData;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 console.log(await getAllContacts());
